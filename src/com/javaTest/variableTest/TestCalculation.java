46ae0feaa599e550df4ede9a411b31127ae890ba
kimdahyeee <dahye4321@naver.com>
2016-11-02 15:47:04 +0900

    20161102 java_연산자

---
+++ src/com/javaTest/variableTest/TestCalculation.java
@@ -0,0 +1,14 @@
+package com.javaTest.variableTest;
+
+import java.util.Scanner;
+
+public class TestCalculation {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int i = scanner.nextInt();
+		
+		System.out.println("i % 3 = "+ (i % 3));
+	}
+	
+}
