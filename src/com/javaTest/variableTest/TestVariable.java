7992017f8b2344f5dd3cfe95d84b48c0845260c8
kimdahye <dahye4321@naver.com>
2016-11-02 15:22:31 +0900

    javaStudy

---
+++ src/com/javaTest/variableTest/TestVariable.java
@@ -0,0 +1,21 @@
+package com.javaTest.variableTest;
+
+//2016.11.02 변수 공부 
+public class TestVariable {
+	public static void main(String[] args) {
+		int i =10;
+		int j=20;
+		int h=30;
+		
+		char c='A';
+		char d = 'a';
+		char e ='B';
+		
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(h);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+}
