37dcffbbf1ceb5c63633d8066a6b6004d063c7a8
kimdahyeee <dahye4321@naver.com>
2016-11-02 22:58:32 +0900

    20161102_메소드

---
+++ src/com/javaTest/variableTest/TestGugudan.java
@@ -0,0 +1,20 @@
+package com.javaTest.variableTest;
+
+import java.util.Scanner;
+
+public class TestGugudan {
+	public static void main(String[] args) {
+		 
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		
+		TestGugudan testG = new TestGugudan();
+		testG.gugudan(input);
+	}
+	
+	public void gugudan(int i){
+		for(int j=1; j<10; j++){
+			System.out.println(i + " * " + j +" = "+ (i*j));
+		}
+	}
+}
