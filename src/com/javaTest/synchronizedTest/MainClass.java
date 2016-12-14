c6d4af7c3be8347cbf401a7d8a113ca75f32df38
kimdahyeee <dahye4321@naver.com>
2016-12-14 18:17:28 +0900

    20161214 syncronized
    다른 스레드 끝냈을 시에만 다른 스레드 실행가능

--- src/com/javaTest/synchronizedTest/MainClass.java
+++ src/com/javaTest/synchronizedTest/MainClass.java
@@ -1,1 +1,17 @@
+package synchronizedTest;
 
+public class MainClass {
+
+	public static void main(String[] args) {
+		SyncTest st = new SyncTest();
+		
+		Thread thread0 = new Thread(st, "A");
+		Thread thread1 = new Thread(st, "B");
+		
+		thread0.start();
+		thread1.start();
+		
+		System.out.println(Thread.currentThread().getName());
+		System.out.println("MainClass");
+	}
+}
