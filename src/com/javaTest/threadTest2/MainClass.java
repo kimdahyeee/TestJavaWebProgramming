27ce03feabea7724b1846f219f638da0dba6f8ed
kimdahyeee <dahye4321@naver.com>
2016-12-14 18:15:21 +0900

    20161214 thread(runnable)

--- src/com/javaTest/threadTest2/MainClass.java
+++ src/com/javaTest/threadTest2/MainClass.java
@@ -1,1 +1,18 @@
+package threadTest2;
 
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		RunnableTest2 rt2 = new RunnableTest2();
+		RunnableTest2 rt3 = new RunnableTest2();
+		
+		Thread thread0 = new Thread(rt2, "A");
+		Thread thread1 = new Thread(rt3, "B");
+		
+		thread0.start();
+		thread1.start();
+		
+		System.out.println(Thread.currentThread().getName());
+	}
+}
---
+++ src/com/javaTest/threadTest2/RunnableTest2.java
@@ -0,0 +1,23 @@
+package threadTest2;
+
+public class RunnableTest2 implements Runnable{
+
+	int testNum = 0;
+	@Override
+	public void run() {
+		for(int i=0; i<10; i++){
+			if(Thread.currentThread().getName().equals("A")){
+				System.out.println("==========================");
+				testNum++;
+			}
+			System.out.println("Thread Name : " + Thread.currentThread().getName() + ", testNum : " + testNum);
+			
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {}
+		}
+	}
+	
+	
+
+}
