f74744c8c583ad59e926510a6de736e82ce83cbf
kimdahyeee <dahye4321@naver.com>
2016-12-14 18:18:57 +0900

    20161214 gui(Button, panal, list...)

--- src/com/javaTest/guiTest2/MainClass.java
+++ src/com/javaTest/guiTest2/MainClass.java
@@ -1,1 +1,14 @@
+package guiTest2;
 
+import java.awt.Dimension;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		MakeFrame makeFrame = new MakeFrame();
+		makeFrame.setSize(new Dimension(500, 500));
+		makeFrame.setVisible(true);
+	}
+
+}
---
+++ src/com/javaTest/guiTest2/MakeFrame.java
@@ -0,0 +1,65 @@
+package guiTest2;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.List;
+import java.awt.Panel;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import sun.awt.WindowClosingListener;
+
+public class MakeFrame extends Frame implements ActionListener{
+	
+	private List list;
+	private Button okBtn;
+	private Button exitBtn;
+	private Panel panel;
+	private TextField textField;
+	
+	public MakeFrame(){
+		list = new List();
+		panel = new Panel();
+		textField = new TextField(20);
+		okBtn = new Button("OK");
+		exitBtn = new Button("EXIT");
+		
+		setLayout(new BorderLayout());
+		add(panel, BorderLayout.NORTH);
+		add(list, BorderLayout.CENTER);
+		
+		panel.add(new Label("write"));
+		panel.add(textField);
+		panel.add(okBtn);
+		panel.add(exitBtn);
+		
+		okBtn.addActionListener(this);
+		exitBtn.addActionListener(this);
+		
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				setVisible(false);
+				dispose();
+				System.exit(0);
+			}
+		});
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == okBtn){
+			list.add(textField.getText());
+		}else if(e.getSource() == exitBtn){
+			setVisible(false);
+			dispose();
+			System.exit(0);
+		}
+	}
+
+}
