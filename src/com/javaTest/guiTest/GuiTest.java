a05a1af0ae11de7dc7a72bea1f8dd3a83e380557
kimdahyeee <dahye4321@naver.com>
2016-12-14 18:18:10 +0900

    20161214 gui(Button)

---
+++ src/com/javaTest/guiTest/GuiTest.java
@@ -0,0 +1,12 @@
+package guiTest;
+
+import java.awt.Button;
+import java.awt.Frame;
+
+public class GuiTest extends Frame{
+
+	public GuiTest() {
+		Button btn = new Button("button");
+		add(btn);
+	}
+}
--- src/com/javaTest/guiTest/MainClass.java
+++ src/com/javaTest/guiTest/MainClass.java
@@ -1,1 +1,28 @@
+package guiTest;
 
+import java.awt.Dimension;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		GuiTest btn = new GuiTest();
+		btn.pack();
+		btn.setSize(new Dimension(200, 200));
+		btn.setVisible(true);
+		
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) {	}
+		
+		btn.setVisible(false);
+		btn.dispose();
+		
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) {}
+		
+		System.exit(0);
+	}
+	
+}
